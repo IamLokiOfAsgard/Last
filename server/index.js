e09e719fb6b145ef2d6f81bdb726939f6b508c89
Deepak <deepakrocking96@gmail.com>
2018-04-14 23:43:46 +0530

    Almost done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const authRoutes=require('./routes/authRoutes')
+require('./models/User')
+require('./models/Survey')
+const passportConfig= require('./services/passport')
 const app =express();
+const mongoose=require('mongoose')
+const cookieSession=require('cookie-session');
+const passport=require('passport');
+const keys=require('./config/keys');
+const bodyParser=require('body-parser');
 
-app.get('/',(req,res)=>{
-	res.send({hey:"bye"})
-})
+app.use(bodyParser.json())
+
+app.use(
+	cookieSession({
+		maxAge:30*24*60*60*1000,
+		keys:[keys.cookieKey]
+	})
+
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongoURI);
+authRoutes(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+
+
 
 app.listen(5000);
